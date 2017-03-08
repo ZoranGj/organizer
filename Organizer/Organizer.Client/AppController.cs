e8f9daad9c8ca7441fe0439aa3c8fb9ff417c4ad
Zoran Gj <Zoran Gj@ZOKI-PC>
2017-03-09 00:29:56 +0100

    Added AngularJs app.js and routing, views, controllers.

--- Organizer/Organizer.Client/AppController.cs
+++ Organizer/Organizer.Client/AppController.cs
@@ -1,5 +1,8 @@
 ﻿using CefSharp;
 using CefSharp.WinForms;
+using Model.DataProviders;
+using Newtonsoft.Json;
+using Organizer.Model;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -33,5 +36,145 @@ namespace Organizer.Client
             ProcessStartInfo start = new ProcessStartInfo("cmd.exe", "/c pause");
             Process.Start(start);
         }
+
+        #region Categories
+
+        public string GetCategories()
+        {
+            var categoryProvider = new CategoriesProvider();
+            var data = categoryProvider.GetAll();
+            return SerializeObject(data);
+        }
+
+        public void AddCategory(string name, int priority)
+        {
+            var id = new Random().Next(10000);
+            var categoryProvider = new CategoriesProvider();
+            categoryProvider.Insert(new Category
+            {
+                Id = id,
+                Name = name,
+                Priority = priority
+            });
+            categoryProvider.Save();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            var categoryProvider = new CategoriesProvider();
+            categoryProvider.Delete(id);
+            categoryProvider.Save();
+        }
+
+        public void UpdateCategoryPriority(int id, int newPriority)
+        {
+            var categoryProvider = new CategoriesProvider();
+            categoryProvider.UpdatePriority(id, newPriority);
+        }
+
+        #endregion
+
+        #region Activities
+
+        public void SaveActivity(int categoryId, string name, string description)
+        {
+            var id = new Random().Next(100000);
+            var activityProvider = new ActivitiesProvider();
+            activityProvider.Insert(new Activity
+            {
+                Id = id,
+                Name = name,
+                Description = description,
+                CategoryId = categoryId,
+                Priority = 2
+            });
+            activityProvider.Save();
+        }
+
+        public void DeleteActivity(int id)
+        {
+            var activityProvider = new ActivitiesProvider();
+            activityProvider.Delete(id);
+            activityProvider.Save();
+        }
+
+        public string GetActivityItems()
+        {
+            var activityProvider = new ActivitiesProvider();
+            var dictionary = new Dictionary<int, string>();
+            return SerializeObject(activityProvider.GetAll().Select(x => new ActivityDto()
+            {
+                Name = x.Name,
+                Id = x.Id
+            }).ToList());
+        }
+
+        #endregion
+
+        public void AddTodoItem(int activityId)
+        {
+            var id = new Random().Next(100000);
+            var todoItemProvider = new TodoItemsProvider();
+            todoItemProvider.Insert(new TodoItem
+            {
+                Id = id,
+                ActivityId = activityId,
+                Deadline = new DateTime(2017, 04, 04).AddMinutes(id + new Random().Next(30)),
+                AddedOn = DateTime.Now.AddMinutes(id),
+                Description = "Simple todo item for activity " + id
+            });
+            todoItemProvider.Save();
+        }
+
+        public string GetTodoItems()
+        {
+            var todoItemProvider = new TodoItemsProvider();
+            var data = todoItemProvider.GetAll();
+            return SerializeObject(data.Select(x => new ToDoItemDto
+            {
+                Id = x.Id,
+                Activity = x.Activity.Name,
+                Description = x.Description,
+                AddedOn = x.AddedOn,
+                Deadline = x.Deadline
+            }));
+        }
+
+        public void DeleteTodoItem(int todoItemId)
+        {
+            var todoItemProvider = new TodoItemsProvider();
+            todoItemProvider.Delete(todoItemId);
+            todoItemProvider.Save();
+        }
+
+        private string SerializeObject(object data)
+        {
+            return JsonConvert.SerializeObject(data, Formatting.Indented,
+                                                new JsonSerializerSettings
+                                                {
+                                                    PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                                                });
+        }
+
+        int randomCategoryId()
+        {
+            var list = new List<int> { 4464, 4907, 3709 };
+            return list.ElementAt(new Random().Next(list.Count));
+        }
+    }
+
+    public class ToDoItemDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime AddedOn { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Activity { get; set; }
+    }
+
+    public class ActivityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ Organizer/Organizer.Client/scripts/application/app.js
@@ -0,0 +1,133 @@
+﻿var app = angular.module('ngApp', []);
+appController.showDevTools();
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: 'templates/index.html',
+            controller: 'HomeController'
+        }).
+        when('/categories', {
+            templateUrl: 'templates/categories.html',
+            controller: 'CategoriesController'
+        }).
+        when('/todos', {
+            templateUrl: 'templates/todos.html',
+            controller: 'TodoItemsController'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
+
+app.controller('HomeController', function ($scope) {
+    $scope.message = 'message from home controller';
+});
+
+app.controller('CategoriesController', function ($scope) {
+    $scope.category = {
+        name: '',
+    }
+
+    $scope.initializeCategories = function () {
+        var categories = appController.getCategories();
+        $scope.categories = JSON.parse(categories);
+
+        $("#categories").sortable({
+            tolerance: 'pointer',
+            revert: 'invalid',
+            placeholder: 'col-md-3 card placeholder',
+            forceHelperSize: true,
+            stop: function(evt, ui) {
+                var priority = $('#categories > div').index(ui.item);
+                var categoryId = $(ui.item).data('id');
+                if (priority > -1) {
+                    appController.updateCategoryPriority(categoryId, priority + 1);
+                }
+            }
+        });
+        $('#newCategory').hide();
+        $scope.category.name = null;
+
+        $scope.activity = {
+            categoryId: null,
+            categoryName: null,
+            name: null,
+            description: null,
+        }
+    }
+
+    $scope.addCategory = function () {
+        $('#newCategory').show();
+        $('#categoryName').focus();
+
+        setTimeout(function () {
+            if ($scope.category.name == null) {
+                $scope.cancelAddingCategory();
+            }
+        }, 5000);
+    }
+
+    $scope.cancelAddingCategory = function() {
+        $('#newCategory').hide();
+        $('#categoryName').val('');
+    }
+
+    $scope.saveCategory = function ($event) {
+        if ($scope.category.name) {
+            var priority = $scope.categories.length + 1;
+            appController.addCategory($scope.category.name.toString(), priority);
+            $scope.initializeCategories();
+        }
+    }
+
+    $scope.deleteCategory = function (id) {
+        appController.deleteCategory(id);
+        $scope.initializeCategories();
+    }
+
+    $scope.showicons = false;
+
+    $scope.saveActivity = function() {
+        appController.saveActivity($scope.activity.categoryId, $scope.activity.name, $scope.activity.description);
+        $scope.initializeCategories();
+    }
+
+    $scope.addActivity = function (categoryId, categoryName) {
+        $scope.activity.categoryId = categoryId;
+        $scope.activity.categoryName = categoryName;
+        $('#activityModal').modal();
+    }
+
+    $scope.deleteActivity = function(id) {
+        appController.deleteActivity(id);
+        $scope.initializeCategories();
+    }
+});
+
+app.controller('TodoItemsController', function($scope) {
+    $scope.todoItem = {
+        ActivityId: 0
+    }
+
+    $scope.initializeTodoItems = function () {
+        var todoItems = appController.getTodoItems();
+        $scope.todoItems = JSON.parse(todoItems);
+
+        var activityItems = appController.getActivityItems();
+        $scope.activityItems = JSON.parse(activityItems);
+    }
+
+    $scope.addTodoItem = function () {
+        if ($scope.todoItem.ActivityId) {
+            appController.addTodoItem($scope.todoItem.ActivityId);
+            $scope.initializeTodoItems();
+        }
+    }
+
+    $scope.deleteTodoItem = function (todoItemId) {
+        appController.deleteTodoItem(todoItemId);
+        $scope.initializeTodoItems();
+    }
+});
