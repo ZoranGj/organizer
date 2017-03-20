0a5303cba0bae8187ea3df6e70c8becaa9d35ad1
ZoranGj <zoran.gjuroski@gmail.com>
2017-03-20 23:23:58 +0100

    Added method for fetching productivity reports.

--- Organizer/Organizer.Client/AppController.cs
+++ Organizer/Organizer.Client/AppController.cs
@@ -215,7 +215,6 @@ namespace Organizer.Client
 
         public static DateTime GetStartOfWeek(DateTime value)
         {
-            // Get rid of the time part first...
             value = value.Date;
             int daysIntoWeek = (int)value.DayOfWeek;
             return value.AddDays(-daysIntoWeek);
